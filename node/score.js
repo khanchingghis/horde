19481f48dbd34616be684232eccab876e5101da7
Chingghis <khanchingghis@gmail.com>
2020-11-17 20:44:10 +0000

    less http, update scorer

--- node/score.js
+++ node/score.js
@@ -9,8 +9,13 @@ const servers = require('./servers')
 const { v4: uuidv4 } = require('uuid');
 
 const myFormID = '1FAIpQLScOi8_7neH_71KM1AuS2PL2ZIs794eNv1u4ZunEz8WFuXwyBg'
-let latestKDAs = {}
+
 let iteration = 0
+const cycleRateMS = 3000
+const logCyleInt = 2
+const logActiveCycleInt = 2
+
+let latestKDAs = {}
 let thisGameId = uuidv4()
 
 function getServerFile(callback) {
@@ -53,30 +58,44 @@ function getServerFile(callback) {
     }
 }
 
-function postScores(server) {
-    servers.getFullServerInfo(server).then(thisServer => {
-        if (!thisServer) return null
-        
-        const playerList = thisServer.playerList
-        const serverInfo = thisServer.serverInfo.ServerInfo
-        if (iteration == 1) latestKDAs.latestMap = serverInfo.mapLabel
+async function postScores(server) {
 
-        const timeNow = new Date()
-        const timeStamp = timeNow.toISOString()
+    //local first, then http
+    const thisServer = await rcon.getServerInfo(server)
+    if (!thisServer) return null
 
-        if (playerList.length > 0) {
-            const allKDASum = sumAllKDA(playerList)
-            if ((allKDASum == 0 && allKDASum != latestKDAs.allKDASum) || serverInfo.mapLabel != latestKDAs.latestMap) {
-                thisGameId = uuidv4()
-                latestKDAs.allKDASum = allKDASum
-            }
-            gFormPost(myFormID, playerList, serverInfo)
-        } else {
-            if (iteration % 2 == 0) {
-                console.log(timeStamp, 'no players...')
-            }
+    let playerList = thisServer.playerList
+    let serverInfo = thisServer.serverInfo.ServerInfo
+
+    const timeNow = new Date()
+    const timeStamp = timeNow.toISOString()
+
+
+    if (iteration == 1) {
+        const fullServerDetails = await servers.getFullServerInfo(server)
+        serverInfo = fullServerDetails.serverInfo.ServerInfo
+        latestKDAs.latestMapLabel = serverInfo.mapLabel
+        latestKDAs.latestMapID = serverInfo.MapLabel
+    }
+
+    if (playerList.length > 0) {
+        const allKDASum = sumAllKDA(playerList)
+
+        if ((allKDASum == 0 && allKDASum != latestKDAs.allKDASum) || serverInfo.MapLabel != latestKDAs.latestMapID) {
+            thisGameId = uuidv4()
+            latestKDAs.allKDASum = allKDASum
+            latestKDAs.latestMapID = serverInfo.MapLabel
+            latestKDAs.isNewRound = true
         }
-    })
+
+            const res = await gFormPost(myFormID, playerList, serverInfo, server)
+
+
+    } else {
+        if (iteration % logCyleInt == 0) {
+            console.log(timeStamp, 'no players...')
+        }
+    }
 }
 
 function sumAllKDA(playerList) {
@@ -89,7 +108,7 @@ function sumAllKDA(playerList) {
     return allKDASum
 }
 
-function gFormPost(formID, playerList, serverInfo) {
+async function gFormPost(formID, playerList, serverInfo, serverCon) {
     const formUrl = `https://docs.google.com/forms/u/0/d/e/${formID}/formResponse`
     const timeNow = new Date()
     const timeStamp = timeNow.toISOString()
@@ -101,43 +120,55 @@ function gFormPost(formID, playerList, serverInfo) {
         const playerID = playerInfo.UniqueId
         const playerName = playerInfo.PlayerName
         const playerCountInt = serverInfo.PlayerCount.split('/')[0]
+        const thisServerName = serverInfo.ServerName
 
         if (latestKDAs[playerID] && latestKDAs[playerID] == kdaSum) {
-            if (iteration % 5 == 0) {
+            if (iteration % logActiveCycleInt == 0) {
                 console.log(timeStamp, 'No KDA change. Skipping post...')
             }
         } else {
             latestKDAs[playerID] = kdaSum
+
+            if (latestKDAs.isNewRound) {
+                const fullServerDetails = await servers.getFullServerInfo(serverCon)
+                latestKDAs.MapLabel = fullServerDetails.serverInfo.ServerInfo.mapLabel
+                latestKDAs.GameMode = fullServerDetails.serverInfo.ServerInfo.gameMode
+                console.log(timeStamp, 'New round!')
+                latestKDAs.isNewRound = false
+            }
+
             const sendObj = {
                 "entry.748779749": playerName,
                 "entry.1044143160": playerID,
                 "entry.1011362435": playerInfo.TeamId,
-                "entry.1974831837": serverInfo.mapLabel,
+                "entry.1974831837": latestKDAs.MapLabel,
                 "entry.530914442": kda[0],
                 "entry.1923937347": kda[1],
                 "entry.1339739528": kda[2],
-                "entry.1078658169": serverInfo.ServerName,
-                "entry.742200735": serverInfo.gameMode,
+                "entry.1078658169": thisServerName,
+                "entry.742200735": latestKDAs.GameMode,
                 "entry.686146210": playerCountInt,
                 "entry.1863816147": thisGameId
             }
 
             axios.get(formUrl, {
                 params: sendObj
-            }).then(x => console.log(timeStamp, `Updated ${playerName} whose score is now ${kda[2]}`))
+            })
+            .then(x => console.log(timeStamp, `Updated ${playerName} whose score is now ${kda[2]}`))
+
+
         }
     }
-
 }
 
 function init() {
     getServerFile((server) => {
         // console.log(JSON.stringify(server))
         setInterval(() => {
-            
+
             postScores(server)
             iteration++
-        }, 5000)
+        }, cycleRateMS)
     })
 }
 
