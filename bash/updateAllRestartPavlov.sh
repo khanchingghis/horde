9efa4467e22c91a9bfa009c1a03a1516021c9a16
Chingghis <khanchingghis@gmail.com>
2020-11-24 11:26:11 +0000

    echos

--- bash/updateAllRestartPavlov.sh
+++ bash/updateAllRestartPavlov.sh
@@ -8,7 +8,9 @@ mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
 remoteGameini="https://docs.google.com/spreadsheets/d/1xTNsaQoXIdYJWLzHBuojIwFnvSRKbVxxZqyrJ5Nr4xA/export?format=csv"
 localGameini="/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini"
 
+echo "Stopping Pavlov Server..."
 systemctl stop pavlov
 cd $localHordeDir"bash/"
 ./updateAll.sh
+echo "Starting Pavlov Server..."
 systemctl start pavlov
