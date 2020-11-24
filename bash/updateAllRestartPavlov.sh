7dc1e40bfa0df359e3ae5c52003a1defdcfc31c9
Chingghis <khanchingghis@gmail.com>
2020-11-24 11:16:44 +0000

    live update script

---
+++ bash/updateAllRestartPavlov.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+localInstallDir="/root/"
+localHordeDir=$localInstallDir"horde/"
+localMapsDir=$localMapsDir"maps/"
+hordeRepo="https://github.com/khanchingghis/horde"
+mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
+remoteGameini="https://docs.google.com/spreadsheets/d/1xTNsaQoXIdYJWLzHBuojIwFnvSRKbVxxZqyrJ5Nr4xA/export?format=csv"
+localGameini="/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini"
+
+systemctl stop pavlovserver
+cd $localHordeDir
+./updateAll.sh
+systemctl start pavlovserver
