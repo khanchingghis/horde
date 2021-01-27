8576d9af8d7c43502a085f7c4b486fddce31720b
Chingghis <khanchingghis@gmail.com>
2021-01-27 20:47:42 +0700

    updateAllInstall with function

--- bash/updateAllInstall.sh
+++ bash/updateAllInstall.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#Installs Service
 
-hordeBashDir="/root/horde/bash"
-serviceName="updateAll"
-serviceFile="$serviceName.service"
-scriptFile="$serviceName.sh"
-echo "copying $hordeBashDir/$serviceFile to /etc/systemd/system/$serviceFile"
-cp $hordeBashDir/$serviceFile /etc/systemd/system/$serviceFile
-systemctl enable $serviceName
-systemctl start $serviceName
+install_service() {
+    hordeBashDir="/root/horde/bash"
+    serviceName=$1
+    serviceFile="$serviceName.service"
+    scriptFile="$serviceName.sh"
+    systemctl stop $serviceName
+    systemctl disable $serviceName
+    echo "copying $hordeBashDir/$serviceFile to /etc/systemd/system/$serviceFile"
+    cp $hordeBashDir/$serviceFile /etc/systemd/system/$serviceFile
+    systemctl enable $serviceName
+    systemctl start $serviceName
+}
+
+install_service "updateAll"
