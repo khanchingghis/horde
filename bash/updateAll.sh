1d1c5f0292e73be78bc3f34ea8c91136b63c1852
Chingghis <khanchingghis@gmail.com>
2021-01-27 20:28:48 +0700

    updater only if changes

--- bash/updateAll.sh
+++ bash/updateAll.sh
@@ -7,12 +7,21 @@ hordeRepo="https://github.com/khanchingghis/horde"
 mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
 localGameini="/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini"
 
-echo "Updating Repo..."
-cd $localInstallDir
-git clone $hordeRepo || cd $localHordeDir && git fetch origin master && git reset --hard origin/master
-chmod +rx -R $localHordeDir
+localCommit=$(git rev-parse @)
+upstreamCommit=$(git rev-parse @{u})
 
-echo "Updating Maps..."
-cd $localMapsDir
-unzip -qq -o "*.zip" -d $mapsLocalFolder
-chmod +rx -R $mapsLocalFolder
+echo "Checking for updates..."
+cd $localHordeDir
+git fetch
+if [ $localCommit == $upstreamCommit ]; then
+    echo "No updates"
+else
+    echo "Updating Repo..."
+    cd $localHordeDir && git fetch origin master && git reset --hard origin/master
+    chmod +rx -R $localHordeDir
+
+    echo "Updating Maps..."
+    cd $localMapsDir
+    unzip -qq -o "*.zip" -d $mapsLocalFolder
+    chmod +rx -R $mapsLocalFolder
+fi
