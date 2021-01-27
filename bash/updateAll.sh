6eb9118f72754608f49e628aae9a2e4ca666b48f
Chingghis <khanchingghis@gmail.com>
2021-01-27 20:39:34 +0700

    check for updates

--- bash/updateAll.sh
+++ bash/updateAll.sh
@@ -7,12 +7,13 @@ hordeRepo="https://github.com/khanchingghis/horde"
 mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
 localGameini="/home/steam/pavlovserver/Pavlov/Saved/Config/LinuxServer/Game.ini"
 
-localCommit=$(git rev-parse @)
-upstreamCommit=$(git rev-parse @{u})
 
 echo "Checking for updates..."
 cd $localHordeDir
 git remote update
+localCommit=$(git rev-parse @)
+upstreamCommit=$(git rev-parse @{u})
+
 if [ $localCommit == $upstreamCommit ]; then
     echo "No updates"
 else
