a5d3d8562a4e2550aa9435af83d7e1c1aedc49d5
Chingghis <khanchingghis@gmail.com>
2021-01-28 14:39:52 +0700

    vultrstartup

--- bash/vultrStartupScript.sh
+++ bash/vultrStartupScript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-localInstallDir="/root/"
-localHordeDir=$localInstallDir"horde/"
+localInstallDir="/root"
+localHordeDir=$localInstallDir"/horde"
+hordeNodeDir=$localHordeDir"/node"
+hordeBashDir=$localHordeDir"/bash"
 hordeRepo="https://github.com/khanchingghis/horde"
 
 echo "Downloading Repo..."
@@ -10,7 +12,6 @@ git clone $hordeRepo
 chmod +rx -R $localHordeDir
 
 echo "Installing Node..."
-hordeNodeDir=$localHordeDir"/node"
 cd $hordeNodeDir
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -18,10 +19,10 @@ sudo apt-get install -y nodejs
 echo "Installing Node Dependencies..."
 npm i
 
-echo "Getting initial settings..."
+echo "Getting Settings and Installing Pavlov..."
 cd $hordeNodeDir
-node makeSettings.js
+node installPavlov.js
 
-echo "Installing Pavlov Server"
-
-echo "Installing Horde Services"
+echo "Installing Horde Services..."
+cd $hordeBashDir
+./installServices.sh
