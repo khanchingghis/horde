403a7ded0f94f3517639fde6617d943d5e29d26e
Chingghis <khanchingghis@gmail.com>
2021-03-10 13:02:16 +0700

    pavlov 2 remove

--- bash/cleanLogs.sh
+++ bash/cleanLogs.sh
@@ -5,4 +5,5 @@ logFile="/home/steam/pavlovserver/Pavlov/Saved/Logs/Pavlov.log"
 backupLogsDir="/home/steam/pavlovserver/Pavlov/Saved/Logs/"
 echo "Cleaning $logFile"
 :>$logFile
-rm -r $backupLogsDir"Pavlov-backup-*"
+rm -r "$backupLogsDir"Pavlov-backup-*
+rm -r "$backupLogsDir"Pavlov_*
