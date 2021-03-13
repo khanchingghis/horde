884f6332b75a045e738023cb5d558b09cc95f5e1
Chingghis <khanchingghis@gmail.com>
2021-03-13 11:24:46 +0700

    cleanbetter

--- bash/cleanLogs.sh
+++ bash/cleanLogs.sh
@@ -2,8 +2,9 @@
 #Wipes Logs
 
 logFile="/home/steam/pavlovserver/Pavlov/Saved/Logs/Pavlov.log"
+logFile2="/home/steam/pavlovserver/Pavlov/Saved/Logs/Pavlov_2.log"
 backupLogsDir="/home/steam/pavlovserver/Pavlov/Saved/Logs/"
 echo "Cleaning $logFile"
 :>$logFile
+:>$logFile2
 rm -r "$backupLogsDir"Pavlov-backup-*
-rm -r "$backupLogsDir"Pavlov_*
