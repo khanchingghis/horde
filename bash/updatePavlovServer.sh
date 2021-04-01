a87efc8fcd3ee8199f7bd3bbb3c23b493e10d6d2
Chingghis <khanchingghis@gmail.com>
2021-04-01 12:40:06 +0700

    sudo steam

--- bash/updatePavlovServer.sh
+++ bash/updatePavlovServer.sh
@@ -5,10 +5,9 @@
 systemctl stop pavlov
 sudo apt update
 sudo apt install -y gdb curl lib32gcc1 libc++-dev unzip
-sudo -u steam /home/steam/Steam/steamcmd.sh +login anonymous +force_install_dir /home/steam/pavlovserver +app_update 622970 -beta shack +exit
-sudo -u steam /home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
+sudo -i -u steam bash << EOF
+/home/steam/Steam/steamcmd.sh +login anonymous +force_install_dir /home/steam/pavlovserver +app_update 622970 -beta shack +exit
+/home/steam/Steam/steamcmd.sh +login anonymous +app_update 1007 +quit
+EOF
 /root/horde/bash/changeGameINI.sh
 systemctl start pavlov
-
-
-
