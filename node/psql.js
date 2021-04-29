9ce7646666e1c16884272ecd5ca5d83b55116aff
Chingghis <khanchingghis@gmail.com>
2021-04-29 11:30:11 +0700

    client to pool

--- node/psql.js
+++ node/psql.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 const score = require('./score')
 const writerPSQLSettings = require('./psqlOptions.json')
 
@@ -20,23 +20,14 @@ async function sendData(psqlSettings, playerList, serverInfo) {
     playerListFormatted = JSON.stringify(playerListFormatted)
     const serverInfoFormatted = JSON.stringify(serverInfo)
 
-    const client = new Client(psqlSettings)
-
-    client.connect().then(x => {
-        client.query(`INSERT INTO raw_scores (playerList, serverInfo) VALUES ($1, $2)`,[playerListFormatted,serverInfoFormatted]).then(y=>{
-            client.end()
-        })
-    })
+    pool.query(`INSERT INTO raw_scores (playerList, serverInfo) VALUES ($1, $2)`,[playerListFormatted,serverInfoFormatted])
+            
 }
 
 async function logData(psqlSettings, clientId, data, result){
-    const client = new Client(psqlSettings)
-
-    client.connect().then(x => {
-        client.query(`INSERT INTO logs (client, body_data, result) VALUES ($1, $2, $3)`,[clientId, data, result]).then(y=>{
-            client.end()
-        })
-    })
+    
+        pool.query(`INSERT INTO logs (client, body_data, result) VALUES ($1, $2, $3)`,[clientId, data, result])
+           
 }
 
 async function writeReport(report){
