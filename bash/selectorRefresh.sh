08b4b94df023550f45cfda649cade5f16603425a
Chingghis <khanchingghis@gmail.com>
2021-05-27 16:30:13 +0700

    elijah aztec

--- bash/selectorRefresh.sh
+++ bash/selectorRefresh.sh
@@ -2,9 +2,10 @@ myip=$(curl icanhazip.com)
 myipTxt=$(echo $myip | tr -d '.')
 myCredsFileName=creds$myipTxt.txt
 pavlovConfigDir="/home/steam/pavlovserver/Pavlov/Saved/Config"
-isAll=${1:-True}
+MapMode=${1:-FavAll}
 
+mkdir -p $pavlovConfigDir/ModSave/
 cp $pavlovConfigDir/RconSettings.txt $pavlovConfigDir/ModSave/$myCredsFileName
 echo IP=$myip >> $pavlovConfigDir/ModSave/$myCredsFileName
-echo All=$isAll >> $pavlovConfigDir/ModSave/$myCredsFileName
+echo Maps=$MapMode >> $pavlovConfigDir/ModSave/$myCredsFileName
 cp $pavlovConfigDir/LinuxServer/Game.ini $pavlovConfigDir/ModSave/Game.txt
