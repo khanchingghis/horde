8486c616a02c4ca7585e1403e19c920a231e6758
Chingghis <khanchingghis@gmail.com>
2021-05-29 17:35:02 +0700

    debug selector update

--- bash/selectorUpdate.sh
+++ bash/selectorUpdate.sh
@@ -4,7 +4,9 @@ myCredsFileName=creds$myipTxt.txt
 pavlovConfigDir="/home/steam/pavlovserver/Pavlov/Saved/Config"
 MapMode=${1:-None}
 
+echo "Updating Selector..."
 mkdir -p $pavlovConfigDir/ModSave/
 cp $pavlovConfigDir/RconSettings.txt $pavlovConfigDir/ModSave/$myCredsFileName
 echo IP=$myip >> $pavlovConfigDir/ModSave/$myCredsFileName
 echo Maps=$MapMode >> $pavlovConfigDir/ModSave/$myCredsFileName
+echo "Updated Selector"
