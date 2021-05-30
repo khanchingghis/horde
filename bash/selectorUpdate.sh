229a44ed33ceefe138c89be7c4f4e3eacb39a875
Chingghis <khanchingghis@gmail.com>
2021-05-30 14:59:46 +0700

    select update

--- bash/selectorUpdate.sh
+++ bash/selectorUpdate.sh
@@ -1,10 +1,12 @@
+echo "Updating Selector..."
+
 myip=$(dig @resolver4.opendns.com myip.opendns.com +short)
 myipTxt=$(echo $myip | tr -d '.')
 myCredsFileName=creds$myipTxt.txt
 pavlovConfigDir="/home/steam/pavlovserver/Pavlov/Saved/Config"
 MapMode=${1:-None}
 
-echo "Updating Selector..."
+echo "Done Variables..."
 mkdir -p $pavlovConfigDir/ModSave/
 cp $pavlovConfigDir/RconSettings.txt $pavlovConfigDir/ModSave/$myCredsFileName
 echo IP=$myip >> $pavlovConfigDir/ModSave/$myCredsFileName
