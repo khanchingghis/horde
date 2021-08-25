aa32cbec81064fed790897a77147fa43d5133bfa
Chingghis <khanchingghis@gmail.com>
2021-08-25 17:04:20 +0700

    async init

--- node/index.js
+++ node/index.js
@@ -1,9 +1,13 @@
 const score = require('./score')
 const logScraper = require('./logScraper')
 
+
+async function initAsync(){
 score.init()
     .then(x => console.log('Crashed:', x))
     .catch(e=>console.log('Crashed:', e))
 
 logScraper.watchLog()
+}
 
+initAsync()
