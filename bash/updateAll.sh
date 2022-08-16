3ea98d6a1f65c3c3356d10ce3a2bf5cbd25cb992
oliverbn <olibn333@gmail.com>
2022-08-16 14:09:35 +0100

    updateAll perms

--- bash/updateAll.sh
+++ bash/updateAll.sh
@@ -17,11 +17,7 @@ upstreamCommit=$(git rev-parse @{u})
 if [ $localCommit == $upstreamCommit ]; then
     echo "No updates"
 else
-    echo "Updating Repo..."
-    cd $localHordeDir && git fetch origin master --depth=1 && git reset --hard origin/master
-    chmod +rx -R $localHordeDir
+    ./bash/updateRepo.sh
 fi
 
-echo "Updating Maps..."
-rclone sync :s3,endpoint=ewr1.vultrobjects.com:hordemaps/ $mapsLocalFolder
-chmod +rx -R $mapsLocalFolder
+./bash/updateMaps.sh
