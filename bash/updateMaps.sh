736f7442a4c2e7b27a5fada98c77e2182f2fb456
Chingghis <khanchingghis@gmail.com>
2023-08-14 19:45:31 +0700

    use gcp for live maps

--- bash/updateMaps.sh
+++ bash/updateMaps.sh
@@ -6,5 +6,6 @@ localMapsDir=$localHordeDir"maps/"
 mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
 
 echo "Updating Maps..."
-rclone sync :s3,endpoint=ewr1.vultrobjects.com:hordemaps/ $mapsLocalFolder
+# rclone sync :s3,endpoint=ewr1.vultrobjects.com:hordemaps/ $mapsLocalFolder
+rclone sync -P :"google cloud storage",anonymous=true,location=us-central1:hordemaps/ $mapsLocalFolder
 chmod 777 -R $mapsLocalFolder
