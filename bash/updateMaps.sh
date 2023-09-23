33084a351b37be8f265c8bad9fa77a708d15fa75
Chingghis <khanchingghis@gmail.com>
2023-09-23 14:13:37 +0700

    sudo apt update

--- bash/updateMaps.sh
+++ bash/updateMaps.sh
@@ -7,5 +7,5 @@ mapsLocalFolder="/home/steam/pavlovserver/Pavlov/Saved/maps/"
 
 echo "Updating Maps..."
 # rclone sync :s3,endpoint=ewr1.vultrobjects.com:hordemaps/ $mapsLocalFolder
-rclone sync -P :"google cloud storage",anonymous=true,location=us-central1:hordemaps/ $mapsLocalFolder
+rclone sync :"google cloud storage",anonymous=true,location=us-central1:hordemaps/ $mapsLocalFolder
 chmod 777 -R $mapsLocalFolder
