eaf4559a1a559de859e047caa3e258c413fc8aac
Chingghis <khanchingghis@gmail.com>
2023-11-05 14:48:20 +0700

    restart dont kill

--- bash/cleanJournal.sh
+++ bash/cleanJournal.sh
@@ -7,5 +7,5 @@ echo "Cleaning $journalConfPath and setting limit to $limit"
 
 rm -r /var/log/journal/*
 sed -i -e "s/#SystemMaxUse=/SystemMaxUse=$limit/" $journalConfPath
-sudo systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service
+# sudo systemctl kill --kill-who=main --signal=SIGUSR2 systemd-journald.service
 systemctl restart systemd-journald
